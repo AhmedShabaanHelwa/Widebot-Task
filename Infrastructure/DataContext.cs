bec70da789295d9abfd4e4aee38e5e261f299e86
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2020-08-04 17:27:11 +0200

    Major Change: 1 - Manually Seeding the database 2 - Add Generic Repository ---
    Infrastructure:
    1 - Migrations for seeding.
    2 - Overriding the OnModelCreating method

--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -15,5 +15,27 @@ namespace Infrastructure
 
         //!AhmedShaban: Define the database tables
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<User>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<User>().HasData( new List<User> {  
+                    new User
+                    { Id = 1, email = "george.bluth@reqres.in", first_name = "George", last_name = "Bluth", avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg" },
+                    new User
+                    { Id = 2, email = "janet.weaver@reqres.in", first_name = "Janet", last_name = "Weaver", avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg" },
+                    new User
+                    { Id = 3, email = "emma.wong@reqres.in", first_name = "Emma", last_name = "Wong", avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg" },
+                    new User
+                    { Id = 4, email = "eve.holt@reqres.in", first_name = "Eve", last_name = "Holt", avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg" },
+                    new User
+                    { Id = 5, email = "charles.morris@reqres.in", first_name = "Charles", last_name = "Morris", avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg" },
+                    new User
+                    { Id = 6, email = "tracey.ramos@reqres.in", first_name = "Tracey", last_name = "Ramos", avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg" }
+                }
+                );
+        }
     }
 }
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Repository;
